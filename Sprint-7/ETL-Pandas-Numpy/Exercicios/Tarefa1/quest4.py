d20626f192b4ccd41a6b89424dabc5584d83a617
Diego Santos <diegocontasantos@gmail.com>
2023-04-21 16:47:49 -0300

    adicionando arquivo do desafio ETL

--- Sprint-7/ETL-Pandas-Numpy/Exercicios/Tarefa1/quest4.py
+++ Sprint-7/ETL-Pandas-Numpy/Exercicios/Tarefa1/quest4.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 arquivo = pd.read_csv("Sprint-7/ETL-Pandas-Numpy/Exercicios/Tarefa1/actors.csv")
 
-frequencia = arquivo["#1 Movie"].value_counts()
-
-for filme, quantidade in frequencia.items():
-    print(f"O filme '{filme}' aparece {quantidade} vez(es) no dataset")
+dfCount = arquivo["#1 Movie"].value_counts().reset_index()
+dfCount.columns = ["Filmes", "Frequência"]
+dfCount = dfCount[dfCount["Frequência"] > 1]
+print(dfCount)
---
+++ Sprint-7/ETL-Pandas-Numpy/desafio-ETL/upload.py
@@ -0,0 +1,27 @@
+import boto3
+import datetime
+
+#colocando em uma variavel o local dos arquivos de upload
+movies_filename = 'movies.csv'
+series_filename = 'series.csv'
+
+#criando uma seção na AWS com o nome do perfil e região
+session = boto3.Session(profile_name="310881667198_AdministratorAccess",region_name="us-east-1")
+
+#criando um objeto para interagir com o serviço S3 da AWS, podendo realizar operações de upload
+s3 = session.resource('s3')
+
+#criando a variavel para mostrar o nome do bucket.
+bucket_name = 'bucket-ingestao-dados'
+
+#criando a variavel para mostrar a data.
+today = datetime.date.today()
+
+#criando a variavel para armazenar o local para ser armazenado na nuvem
+movies_path = f'Raw/Local/CSV/Movies/{today.year}/{today.month}/{today.day}/movies.csv'
+series_path = f'Raw/Local/CSV/Series/{today.year}/{today.month}/{today.day}/series.csv'
+
+#processo de upload para a AWS
+s3.meta.client.upload_file(movies_filename, bucket_name, movies_path)
+s3.meta.client.upload_file(series_filename, bucket_name, series_path)
+
