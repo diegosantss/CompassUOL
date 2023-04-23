479d7203424e33e7bdd39ca95ec838e0530f92f1
Diego Santos <diegocontasantos@gmail.com>
2023-04-23 14:56:59 -0300

    enviando resultado do exercicio tarefa 2

--- Sprint-7/ETL-Pandas-Numpy/Exercicios/Tarefa2/contador.py
+++ Sprint-7/ETL-Pandas-Numpy/Exercicios/Tarefa2/contador.py
@@ -1,6 +1,6 @@
 # faça um programa que conte as palavras de um arquivo README.md
 
-with open('Sprint-7/ETL-Pandas-Numpy/Exercicios/Tarefa2/README.md', 'r') as f:
+with open('README.md', 'r') as f:
     arquivo = f.read()
 
 palavras = arquivo.split()
