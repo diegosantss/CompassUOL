e3ba6791ef0b8eca9183ebd80e6cda59f38ae907
Diego Santos <diegocontasantos@gmail.com>
2023-04-24 10:15:51 -0300

    adicionando comentário no códig python de upload para aws

--- Sprint-7/ETL-Pandas-Numpy/desafio-ETL/upload.py
+++ Sprint-7/ETL-Pandas-Numpy/desafio-ETL/upload.py
@@ -14,13 +14,13 @@ s3 = session.resource('s3')
 #criando a variavel para mostrar o nome do bucket.
 bucket_name = 'bucket-ingestao-dados'
 
-#criando a variavel para mostrar a data.
+#criando a variavel para instanciar a data atual no caminho do upload.
 today = datetime.date.today()
 
 #criando a variavel para armazenar o local para ser armazenado na nuvem
 movies_path = f'Raw/Local/CSV/Movies/{today.year}/{today.month}/{today.day}/movies.csv'
 series_path = f'Raw/Local/CSV/Series/{today.year}/{today.month}/{today.day}/series.csv'
 
-#processo de upload para a AWS
+#processo de upload para a AWS dos arquivos Movies.csv e Series.csv
 s3.meta.client.upload_file(movies_filename, bucket_name, movies_path)
 s3.meta.client.upload_file(series_filename, bucket_name, series_path)
